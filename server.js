96a7ac39d79bd5b7b74f9dd41795d7bb3a79d0f8
Nadja Brandys <nadiusza@gmail.com>
2020-07-21 23:36:14 +0200

    Modify endpoints

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+
 const mongoClient = require('mongodb').MongoClient;
 
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
+mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err){
+    console.log(err);
+  }
+  else {
+    console.log('Successfully connected to the database');
+    const db = client.db('companyDB');
+    const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+});
+//middleware for external endpoints
 app.use('/api', employeesRoutes);
 app.use('/api', departmentsRoutes);
 app.use('/api', productsRoutes);
@@ -21,4 +34,7 @@ app.use((req, res) => {
 
 app.listen('8000', () => {
   console.log('Server is running on port: 8000');
+});
+
+}
 });
